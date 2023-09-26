87168bbe96e63c1f4a9cecf4da67757652f79e98
Bruno Henrique <brunohenriquex00@gmail.com>
2023-09-26 18:17:55 -0300

    removendo importacoes

--- src/main/java/one/digital/innovation/personaapi/service/PersonService.java
+++ src/main/java/one/digital/innovation/personaapi/service/PersonService.java
@@ -5,8 +5,6 @@ import one.digital.innovation.personaapi.entity.Person;
 import one.digital.innovation.personaapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class PersonService {
